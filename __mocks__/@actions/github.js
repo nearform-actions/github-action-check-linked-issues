35f3af96b299b0a9e4483d97603527acda76e61b
Gerjan Stokkink <gerjan@bookingexperts.com>
2025-05-22 12:23:47 +0200

    Improve Github keyword matching (#674)
    Github allows additional spacing and a single colon character between the Github keyword and the issue number.

--- __mocks__/@actions/github.js
+++ __mocks__/@actions/github.js
@@ -16,7 +16,7 @@ module.exports = {
               node_id: "fake-node-id",
               body: '<!-- metadata = {"action":"linked_issue"} -->',
             },
-          ])
+          ]),
         );
       }),
       graphql: jest.fn(() => {
@@ -28,53 +28,53 @@ module.exports = {
               pullRequest: {
                 id: "fake-pr-id",
                 body: github.context.externalRepo
-                  ? "Lorem ipsum close https://github.com/orgone/repoone/issues/123, fixes https://github.com/orgtwo/repotwo/issues/456 and closes ext_org/ext_repo#1337"
+                  ? "Lorem ipsum close https://github.com/orgone/repoone/issues/123, fixes  https://github.com/orgtwo/repotwo/issues/456 and closes: ext_org/ext_repo#1337"
                   : github.context.localRepo
-                    ? "Lorem ipsum close #12345 and fix #456"
-                    : "Lorem ipsum close https://github.com/orgone/repoone/issues/123, fixes https://github.com/orgtwo/repotwo/issues/456 and resolves ext_org/ext_repo#1337 but also closes #12345 and fixed #67890",
+                    ? "Lorem ipsum close #12345 and fix: #456"
+                    : "Lorem ipsum close https://github.com/orgone/repoone/issues/123, fixes  :  https://github.com/orgtwo/repotwo/issues/456 and resolves ext_org/ext_repo#1337 but also closes #12345 and fixed #67890",
                 closingIssuesReferences: github.context.looseMatching
-                    ? {}
-                    : {
-                  totalCount: 2,
-                  nodes: [
-                    {
-                      number: 12345,
-                      repository: {
-                        nameWithOwner: "test/repo",
-                      },
+                  ? {}
+                  : {
+                      totalCount: 2,
+                      nodes: [
+                        {
+                          number: 12345,
+                          repository: {
+                            nameWithOwner: "test/repo",
+                          },
+                        },
+                        {
+                          number: 456,
+                          repository: {
+                            nameWithOwner: "another/repository",
+                          },
+                        },
+                      ],
                     },
-                    {
-                      number: 456,
-                      repository: {
-                        nameWithOwner: "another/repository",
-                      },
-                    },
-                  ],
-                },
                 labels: github.context.withNoIssueLabel
                   ? {
-                    nodes: [
-                      {
-                        name: "foo",
-                      },
-                      {
-                        name: "no-issue",
-                      },
-                      {
-                        name: "bar",
-                      }
-                    ],
-                  }
+                      nodes: [
+                        {
+                          name: "foo",
+                        },
+                        {
+                          name: "no-issue",
+                        },
+                        {
+                          name: "bar",
+                        },
+                      ],
+                    }
                   : {
-                    nodes: [
-                      {
-                        name: "foo",
-                      },
-                      {
-                        name: "bar",
-                      }
-                    ],
-                  },
+                      nodes: [
+                        {
+                          name: "foo",
+                        },
+                        {
+                          name: "bar",
+                        },
+                      ],
+                    },
               },
             },
           });
